e7324b71306b6941b95451068e478f1f375bc5a7
MonsieurBarti <pierre.le-corff@epita.fr>
2020-02-24 14:44:51 +0100

    clear commad

--- bot.py
+++ bot.py
@@ -8,31 +8,39 @@ import requests
 load_dotenv()
 token = os.getenv('BOT_TOKEN')
 description = '''Monsieur Barti\'s Discord Bot'''
-bot = commands.Bot(command_prefix=('$','!','`'), description=description, self_bot=True)
+prefix = ('$','!','`')
+client = commands.Bot(command_prefix=prefix, description=description)
 
-@bot.event
+@client.event
 async def on_ready():
     print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
+    print(client.user.name)
+    print(client.user.id)
     print('------')
 
-@bot.command(name="bot", description="Call the bot")
+@client.command(name="bot", description="Call the bot")
 async def on_message(ctx):
     await ctx.send('What do you need {0}?'.format(ctx.author.mention))
 
-@bot.command(name="bitcoin", description="Ask for the bitcoin market value")
+@client.command(name="bitcoin", description="Ask for the bitcoin market value")
 async def bitcoin_value(ctx):
     url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
     response = requests.get(url)
     value = response.json()['bpi']['USD']['rate']
     await ctx.send('Bitcoin value: {0}$'.format(value))
 
-@bot.command(pass_context=True)
-async def clear(ctx, limit: int=None):
-    async for msg in bot.logs_from(ctx.message.channel, limit=limit):
-        await bot.delete_message (msg)
-    embed = discord.Embed(description="Action completed! :smile:", color=0x00ff00)
-    await bot.say (embed=embed)
+@client.command(name="clear", description="Clear a certain number of messages (!clear X)" ,pass_context=True)
+async def clear(ctx, amount):
+    messages = []
+    async for msg in ctx.history():
+        await msg.delete()
+    embed = discord.Embed(description="Cleared {} messages ! :smile:".format(amount), color=0x00ff00)
+    await ctx.send(embed=embed)
 
-bot.run(token)
+@client.event
+async def on_member_join(member):
+    role = get_role(member)
+    await member.add_role(role)
+    print(f"{member} was given {role}")
+
+client.run(token)
